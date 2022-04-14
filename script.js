d1a881b72867adac8946a0358b373cd2ca03c8dd
qureshi <ishrat.qureshi@pwc.com>
2022-04-14 22:13:37 +0500

    changes includes: leftover(hours,mins.sec) rewrite

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-const AUCTION_DATE = new Date('14 april, 2022');
-const MILLI_SECONDS_IN_DAY = 1000 * 60 * 60 * 24; //86400000
-const MILLI_SECONDS_IN_AN_HOUR = 1000 * 60 * 60; //3600000
-const MILLI_SECONDS_IN_A_MINUTE = 1000 * 60; //60000
+const AUCTION_DATE = new Date('1 jan,2023');
+
+
+const SECONDS_IN_AN_HOUR = 60 * 60; //3600
 const MILLI_SECONDS_IN_A_SECOND = 1000;
+const HOURS_IN_A_DAY = 24;
+const SECONDS_IN_A_MINUTE = 60;
 const INTERVAL = 1000; // miliseconds in one seconds
 
 const counterUpdate = () => {
 	const TODAY_DATE = new Date();
-	const daysLeft = getDifferenceInDays(TODAY_DATE);
-	const hoursLeft = getDifferenceInHours(TODAY_DATE);
-	const minutesLeft = getDifferenceInMinutes(TODAY_DATE);
-	const secondsLeft = getDifferenceInSeconds(TODAY_DATE);
+	const TOTAL_SECONDS = Math.floor(AUCTION_DATE - TODAY_DATE) / 1000;
+	const daysLeft = getDifferenceInDays(TOTAL_SECONDS);
+	const hoursLeft = getDifferenceInHours(TOTAL_SECONDS);
+	const minutesLeft = getDifferenceInMinutes(TOTAL_SECONDS);
+	const secondsLeft = getDifferenceInSeconds(TOTAL_SECONDS);
+	
 	const dayDiv = document.getElementById('days');
-	const minutesDiv = document.getElementById('hours');
-	const hoursDiv = document.getElementById('minutes');
+	const minutesDiv = document.getElementById('minutes');
+	const hoursDiv = document.getElementById('hours');
+
 	const secondsDiv = document.getElementById('seconds');
 	dayDiv.innerHTML = daysLeft;
 	hoursDiv.innerHTML = hoursLeft;
@@ -21,37 +26,21 @@ const counterUpdate = () => {
 	secondsDiv.innerHTML = secondsLeft;
 };
 
-function getDifferenceInDays(
-	todayDate = TODAY_DATE,
-	auctionDate = AUCTION_DATE
-) {
-	const diffInMs = Math.abs(auctionDate - todayDate);
-	return Math.round(diffInMs / MILLI_SECONDS_IN_DAY);
+function getDifferenceInDays(dateDifference) {
+	return Math.floor(dateDifference / MILLI_SECONDS_IN_AN_HOUR / HOURS_IN_A_DAY);
 }
 
-function getDifferenceInHours(
-	todayDate = TODAY_DATE,
-	auctionDate = AUCTION_DATE
-) {
-	const diffInMs = Math.abs(auctionDate - todayDate);
-	return Math.round(diffInMs / MILLI_SECONDS_IN_AN_HOUR);
+function getDifferenceInHours(dateDifference) {
+	return Math.floor(dateDifference / SECONDS_IN_AN_HOUR) % HOURS_IN_A_DAY;
 }
 
-function getDifferenceInMinutes(
-	todayDate = TODAY_DATE,
-	auctionDate = AUCTION_DATE
-) {
-	const diffInMs = Math.abs(auctionDate - todayDate);
-	return Math.round(diffInMs / MILLI_SECONDS_IN_A_MINUTE);
+function getDifferenceInMinutes(dateDifference) {
+
+	return Math.floor(dateDifference / SECONDS_IN_A_MINUTE) % SECONDS_IN_A_MINUTE;
 }
 
-function getDifferenceInSeconds(
-	todayDate = TODAY_DATE,
-	auctionDate = AUCTION_DATE
-) {
-	const diffInMs = Math.abs(auctionDate - todayDate);
-	console.log('diffInMs', diffInMs);
-	return Math.round(diffInMs / MILLI_SECONDS_IN_A_SECOND);
+function getDifferenceInSeconds(dateDifference) {
+	return Math.floor(dateDifference) % SECONDS_IN_A_MINUTE;
 }
 
 counterUpdate();
